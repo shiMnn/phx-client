7a80701850ff76f9e12831f55728d20393c06c32
shiMnn-PC\shiMnn <shimnn0501513@gmail.com>
2017-11-11 00:46:03 +0900

    UniRXの導入

---
+++ Assets/Scripts/Game/GameMain/Root.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Root : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
